fc519650c135f774ff4fc0b46ed09a057eee4e44
Carlos Vivas <carsed@hotmail.com>
2023-07-23 20:41:13 -0400

    Add array tabla IMC

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+// Constante array para almacenar los valores de la tabla de IMC
+const tablaIMC = [];
+
+// Método para llenar la tabla de valores IMC
+function addIMC(valorMin, valorMax, mensaje) {
+  const IMC = {
+    id: tablaIMC.length,
+    minVal: valorMin,
+    maxVal: valorMax,
+    msg: mensaje,
+  };
+
+  tablaIMC.push(IMC);
+}
+
+//Se llena la tabla de IMC con los valores correspondientes
+addIMC(-Infinity, 16, "Delgadez Severa");
+addIMC(16, 17, "Delgadez Moderada");
+addIMC(17, 18.5, "Delgadez Leve");
+addIMC(18.5, 25, "Normal");
+addIMC(25, 30, "Exceso de peso");
+addIMC(30, 35, "Obesidad Clase I");
+addIMC(35, 40, "Obesidad Clase II");
+addIMC(40, Infinity, "Obesidad Clase III");
+
 // Función para obtener y validar el valor de la variable para continuidad de los cálculos
 function validaResp(msg) {
   let resp = prompt(msg);
